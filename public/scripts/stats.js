1590965e467621303ef961f2b310f7ce7c76a0a2
Mitchell Nelson <nels4929@stthomas.edu>
2019-05-15 15:29:15 -0500

    live updates

--- public/scripts/stats.js
+++ public/scripts/stats.js
@@ -1,3 +1,4 @@
+var ws
 var app;
 var username;
 
@@ -24,7 +25,32 @@ function init()
 		}
 	});
 		
+    ws = new WebSocket("ws://" + window.location.hostname + ":" + 8016);
+    ws.onopen = (event) => {
+        console.log("Connection successful!");
+    };
+    ws.onmessage = (event) => {
+        console.log(event.data);
+    	var message = JSON.parse(event.data);
+        if(message.msg === "GAMEOVER"){
+    	
+            $.getJSON('/scores').then((data) =>{
+            console.log(data);
 
+            for(var i = 0; i < data.length; ++i)
+            {
+                if(data[i].username == username)
+                {
+                    app.avatar = data[i].avatar;
+                    app.apples_eaten = data[i].apples_eaten;
+                    app.games_played = data[i].games_played;
+                    app.high_score = data[i].high_score;
+                }
+            }
+
+	        }, 'json');
+        }
+    };
 
 
 	username = params[1].substring(9);
